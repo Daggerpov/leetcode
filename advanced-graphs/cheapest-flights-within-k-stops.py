0d49671b04f41f6d72cf5fcf3ec3cb16cc984f47
Daggerpov <danielagapov1@gmail.com>
2026-02-13 02:40:27 -0800

    Update cheapest-flights-within-k-stops.py

--- advanced-graphs/cheapest-flights-within-k-stops.py
+++ advanced-graphs/cheapest-flights-within-k-stops.py
@@ -1,1 +1,22 @@
-# TODO: solve
+from typing import List
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int):
+        prices = [float("inf")] * n
+        prices[src] = 0
+
+        for i in range(k+1): # k + 1 since start doesn't count as a stop
+            tempPrices = prices.copy()
+
+            for source, destination, price in flights:
+                if prices[source] == float("inf"):
+                    continue
+                curCost = prices[source] + price
+                if curCost < tempPrices[destination]:
+                    tempPrices[destination] = curCost
+
+            prices = tempPrices
+
+        return prices[dst] if prices[dst] != float("inf") else -1
+
